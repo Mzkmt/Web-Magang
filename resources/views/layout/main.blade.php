e303225460b4aa499aee10cc0f80fcef44dbea25
POD <pod@POD.local>
2020-10-05 10:30:58 +0700

    membuat navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,10 +12,10 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark ">
         <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-        <a class="navbar-brand" href="/">
-          <img src="{{asset('storage/image/logoekspresa.png')}}" width="15%" height="15%" alt="">
+        <a class="navbar-brand" href="{{ url('/') }}">
+          <img  src="{{asset('storage/image/logo.png')}}" width="" height="50px" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -26,9 +26,20 @@
             <a class="nav-link" href="/aboutus">About us</a>
             <a class="nav-link" href="#">Product</a>
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
-            <a class="nav-link active" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+            <!-- <a class="nav-link active" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
             <a class="nav-link" href="{{ url('/aboutus') }}">About us</a>
-            <a class="nav-link" href="#">Product</a>
+            <a class="nav-link" href="#">Product</a> -->
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link active" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/aboutus') }}">About us</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#">Product</a>
+              </li>
+            </ul>
             
             </div>
         </div>  
@@ -42,5 +53,21 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script>
+      $(window).scroll(function(){
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 1);
+        $('.navbar-dark .navbar-nav > ul > li > a').toggleClass('scrolled', $(this).scrollTop() > 1);
+        // scroll_pos = $(this).scrollTop();
+        //   if(scroll_pos > 1) {
+        //     $("nav").css('background-color', 'blue');
+        //     $("nav ul li a").css('color', 'black');
+        //   } else {
+        //     $("nav").css('background-color', 'red');
+        //     $("nav ul li a").css('color', 'white');
+        //   }
+        // }
+
+      });
+    </script>
   </body>
 </html>
